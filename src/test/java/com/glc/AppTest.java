ae67c7e03cac71f8887fcc2ac9d2194173c7e517
Shavarsh Shahoyan <shavarsh.sh@gmail.com>
2022-04-29 14:51:10 +0400

    AC3, no RED phase because of numberRead method already was written, GREEN phase

--- src/test/java/com/glc/AppTest.java
+++ src/test/java/com/glc/AppTest.java
@@ -36,4 +36,28 @@ public class AppTest
     	//ASSERT
     	assertEquals(1, cut.numberRead());
     }
+    
+   // Given that I have an empty list, when I add additional books to the list I expect the numberRead to return the total number of books in my list
+    @Test
+    public void afterAddingAdditionalBooksNumberReadShouldReturnCorrectNumberOfBooks()
+    {
+    	//SETUP
+    	ReadingList cut = new ReadingList();
+    	
+    	Book book1 = new Book("Art of Loving","Erich Fromm",133,4);
+    	Book book2 = new Book("Art of Loving","Erich Fromm",133,4);
+    	Book book3 = new Book("Art of Loving","Erich Fromm",133,4);
+    	Book book4 = new Book("Art of Loving","Erich Fromm",133,4);
+    	
+    	cut.addBook(book1,"January 1", 4);
+    	cut.addBook(book2,"January 1", 4);
+    	cut.addBook(book3,"January 1", 4);
+    	cut.addBook(book4,"January 1", 4);
+    	
+    	//Execute
+    	int numberOfBooks = cut.numberRead();
+    	
+    	//ASSERT
+    	assertEquals(4,numberOfBooks);
+    }
 }
